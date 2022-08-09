da0779bd0e0459e7b6c97a0b442b09e04df8f51e
kinath <kinath.ru@gmail.com>
2022-08-09 11:12:34 -0500

    feat:initial implementation

---
+++ CSharpPythonIntegrationDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+string path = Environment.ExpandEnvironmentVariables("%PythonHome%");
+Console.WriteLine(path);
+string arguments = @"sample_script.py";
+var cmd = $"{path}\\python.exe";
+
+ProcessStartInfo start = new ProcessStartInfo();
+start.FileName = cmd;//cmd is full path to python.exe
+start.Arguments = arguments;//args is path to .py file and any cmd line args
+start.UseShellExecute = false;
+start.RedirectStandardOutput = true;
+using(Process process = Process.Start(start))
+{
+    using(StreamReader reader = process.StandardOutput)
+    {
+        string result = reader.ReadToEnd();
+        Console.Write(result);
+    }
+}
